0586944cdc1c0390a42d7485b64e34ce57e925af
osama <osamakhtar@gmail.com>
2018-08-21 17:21:08 +0200

    Tercer Commit

--- src/test/java/com/privalia/dao/integration/test/AlumnoDAOTest.java
+++ src/test/java/com/privalia/dao/integration/test/AlumnoDAOTest.java
@@ -3,22 +3,59 @@ package com.privalia.dao.integration.test;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.junit.Test;
 
 import com.privalia.common.Alumno;
 import com.privalia.dao.AlumnoDAO;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(value = Parameterized.class)
+
 public class AlumnoDAOTest {
+	
+	private int idAlumno;
+	private String nombre;
+	private String apellidos;
+	private String dni;
+	
+	
+
+	public AlumnoDAOTest(int idAlumno, String nombre, String apellidos, String dni) {
+		super();
+		this.idAlumno = idAlumno;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.dni = dni;
+	}
+	
+	@Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {1, "Osama","Khollala","47952183J"},
+                {2, "asdsad","asdasdsa","asdasdasd"},
+                {8, "sadsadsad","sdsadasd","5213645K"},
+                {4, "dsadsadsada","asdasdsa","2136542L"},
+                {5, "sadsadasd","sadsadasd","564564K"}
+        });
+    }
+
+
 
 	@Test
 	public void testAdd() throws UnsupportedOperationException, IOException{
 		
-		Alumno alumno = new Alumno(3,"Pepe","awsdsd","5466454J");
+		Alumno alumno = new Alumno(idAlumno,nombre,apellidos,dni);
 		
 		AlumnoDAO dao = new AlumnoDAO();
 		
-			
 		Alumno alumnoInserted = dao.add(alumno);
 		assertTrue(alumnoInserted.equals(alumno));
 		
